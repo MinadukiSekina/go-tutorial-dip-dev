91fad5bc04ed8ed104d03ab73c9b916a2bf19615
MinadukiSekina <taroufujimoto075@gmail.com>
2025-03-04 21:38:57 +0900

    feat: implement chapter1

--- internal/chapter1/chapter1.go
+++ internal/chapter1/chapter1.go
@@ -1,17 +1,34 @@
 package chapter1
 
-import "net/http"
-
-// revive:disable
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func GetEcho(w http.ResponseWriter, r *http.Request) {
 	//FIXME: Getメソッドのアクセスか確認
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
 	//FIXME: パラメータをFormに変換する
+	if err := r.ParseForm(); err != nil {
+		return
+	}
 
 	//FIXME: パラメータを取得する
+	var ps = map[string]string{}
+	for k, v := range r.Form {
+		ps[k] = v[0]
+	}
 
 	//FIXME: パラメータをレスポンスに書き出す
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(ps); err != nil {
+		return
+	}
 
 	//FIXME: レスポンスコード設定
+	w.WriteHeader(http.StatusOK)
 }
