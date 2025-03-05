249cbf3ccb5a3205810c83c8c3886b05de666579
MinadukiSekina <taroufujimoto075@gmail.com>
2025-03-05 16:29:12 +0900

    test: chapter2: add a test case for the Get() function.

--- internal/chapter2/chapter2_test.go
+++ internal/chapter2/chapter2_test.go
@@ -1,11 +1,63 @@
 package chapter2
 
-import "testing"
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
 
-// revive:disable
+	"github.com/stretchr/testify/assert"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
 
 func TestMain(m *testing.M) {
+	m.Run()
 }
 
 func TestGet(t *testing.T) {
+	success := map[string]struct {
+		params     map[string]string
+		response   []User
+		wantStatus int
+	}{
+		"正常ケース": {
+			params: map[string]string{
+				"age": "25",
+			},
+			response: []User{
+				{
+					Name: "dip 太郎",
+					Age:  25,
+				},
+				{
+					Name: "dip 花子",
+					Age:  25,
+				},
+			},
+			wantStatus: http.StatusOK,
+		},
+	}
+	for tn, tc := range success {
+		t.Run(tn, func(t *testing.T) {
+			param := url.Values{}
+			for k, v := range tc.params {
+				param.Add(k, v)
+			}
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "http://localhost/?"+param.Encode(), nil)
+			Get(w, r)
+			got := []User{}
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Errorf("errpr: %#v, res: %#v", err, got)
+			}
+			assert.Equal(t, tc.wantStatus, w.Code)
+			assert.ElementsMatch(t, got, tc.response)
+		})
+	}
 }
