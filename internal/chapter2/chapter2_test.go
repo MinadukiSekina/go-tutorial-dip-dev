da88a3850da27aa65307da102bd081c44f00c564
MinadukiSekina <taroufujimoto075@gmail.com>
2025-03-12 19:43:07 +0900

    test: chapter2: Add error-handling test cases

--- internal/chapter2/chapter2_test.go
+++ internal/chapter2/chapter2_test.go
@@ -95,6 +95,43 @@ func TestCreate(t *testing.T) {
 			wantStatus: http.StatusOK,
 		},
 	}
+	fail := map[string]struct {
+		method     string
+		params     map[string]string
+		wantStatus int
+	}{
+		"異常ケース：Getメソッド": {
+			method:     http.MethodGet,
+			wantStatus: http.StatusMethodNotAllowed,
+		},
+		"異常ケース：パラメータが不正（nameが空）": {
+			params:     map[string]string{"name": "", "age": "24"},
+			wantStatus: http.StatusBadRequest,
+		},
+		"異常ケース：パラメータが不正（ageが空）": {
+			params:     map[string]string{"name": "dip 次郎", "age": ""},
+			wantStatus: http.StatusBadRequest,
+		},
+		"異常ケース：パラメータが不正（ageが文字列）": {
+			params:     map[string]string{"name": "dip 次郎", "age": "twenty"},
+			wantStatus: http.StatusBadRequest,
+		},
+		"異常ケース：パラメータが不正（すべて空）": {
+			params:     map[string]string{"name": "", "age": ""},
+			wantStatus: http.StatusBadRequest,
+		},
+		"異常ケース：パラメータが不正（name無し）": {
+			params:     map[string]string{"age": "24"},
+			wantStatus: http.StatusBadRequest,
+		},
+		"異常ケース：パラメータが不正（age無し）": {
+			params:     map[string]string{"name": "dip 次郎"},
+			wantStatus: http.StatusBadRequest,
+		},
+		"異常ケース：パラメータなし": {
+			wantStatus: http.StatusBadRequest,
+		},
+	}
 
 	for tn, tc := range success {
 		t.Run(tn, func(t *testing.T) {
@@ -117,6 +154,27 @@ func TestCreate(t *testing.T) {
 			}
 			assert.Equal(t, tc.wantStatus, w.Code)
 			assert.Equal(t, tc.response, got)
+		})
+	}
+	for tn, tc := range fail {
+		t.Run(tn, func(t *testing.T) {
+			params, err := json.Marshal(tc.params)
+			if err != nil {
+				t.Errorf("error: %#v", err)
+				return
+			}
+
+			w := httptest.NewRecorder()
+			var r *http.Request
+			if tc.method == http.MethodGet {
+				r = httptest.NewRequest(tc.method, "http://localhost/?", nil)
+			} else {
+				r = httptest.NewRequest(http.MethodPost, "http://localhost/", bytes.NewReader(params))
+			}
+			
+			Create(w, r)
+
+			assert.Equal(t, tc.wantStatus, w.Code)
 		})
 	}
 }
