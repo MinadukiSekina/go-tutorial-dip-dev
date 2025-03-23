d84cd769edc52f372628d06a7bd54dad7fb9790f
MinadukiSekina <taroufujimoto075@gmail.com>
2025-03-23 11:45:25 +0900

    feat: chapter3: add entry struct and test case for Get()

--- internal/chapter3/chapter3.go
+++ internal/chapter3/chapter3.go
@@ -2,6 +2,12 @@ package chapter3
 
 import "net/http"
 
+type Entry struct {
+	name   string
+	userID int
+	salary int
+}
+
 // revive:disable
 
 func Get(w http.ResponseWriter, r *http.Request) {
--- internal/chapter3/chapter3_test.go
+++ internal/chapter3/chapter3_test.go
@@ -1,11 +1,65 @@
 package chapter3
 
-import "testing"
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
 
-// revive:disable
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMain(m *testing.M) {
+	m.Run()
 }
 
 func TestGet(t *testing.T) {
+	// レスポンスに含まれるキーの名前
+	keyString := "entries"
+
+	success := map[string]struct {
+		params     map[string]string
+		response   []Entry
+		wantStatus int
+	}{
+		"正常ケース": {
+			params: map[string]string{
+				"name": "dip 太郎",
+			},
+			response: []Entry{
+				{
+					name:   "案件情報1",
+					userID: 123456,
+					salary: 123456,
+				},
+				{
+					name:   "案件情報2",
+					userID: 234567,
+					salary: 123456,
+				},
+			},
+			wantStatus: http.StatusOK,
+		},
+	}
+	for tn, tc := range success {
+		t.Run(tn, func(t *testing.T) {
+			param := url.Values{}
+			for k, v := range tc.params {
+				param.Add(k, v)
+			}
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "http://localhost/?"+param.Encode(), nil)
+			Get(w, r)
+			got := map[string][]Entry{}
+			t.Logf(w.Body.String())
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Errorf("error: %#v, res: %#v", err, got)
+			}
+			assert.Equal(t, tc.wantStatus, w.Code)
+			assert.Contains(t, keyString, got)
+			assert.ElementsMatch(t, got[keyString], tc.response)
+		})
+	}
 }
