0631f82f7c90cd7a167322ecf4d7f2af1a54e77d
MinadukiSekina <taroufujimoto075@gmail.com>
2025-03-26 12:36:07 +0900

    test: chapter3: update TestGetUser to use context and modify parameter types for improved API response handling

--- internal/chapter3/chapter3_test.go
+++ internal/chapter3/chapter3_test.go
@@ -1,6 +1,7 @@
 package chapter3
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -85,25 +86,22 @@ func TestGet(t *testing.T) {
 	}
 }
 
-/*
 func TestGetUser(t *testing.T) {
 	success := map[string]struct {
-		params     map[string]string
-		response   []Entry
-		wantStatus int
+		params   map[string][]string
+		response []int
 	}{
-		"正常ケース": {
-			params: map[string]string{
-				"name": "dip 太郎",
+		"正常ケース：データあり": {
+			params: map[string][]string{
+				"name": {"dip 太郎"},
 			},
-			response: []Entry{
-				{
-					Name:   "案件情報1",
-					UserID: 123456,
-					Salary: 123456,
-				},
+			response: []int{123456},
+		},
+		"正常ケース：データなし": {
+			params: map[string][]string{
+				"name": {"dip 三郎"},
 			},
-			wantStatus: http.StatusOK,
+			response: []int{},
 		},
 	}
 	for tn, tc := range success {
@@ -115,10 +113,6 @@ func TestGetUser(t *testing.T) {
 					Path:    "/users",
 					Handler: MockGetUser,
 				},
-				{
-					Path:    "/entries",
-					Handler: MockGetEntry,
-				},
 			}
 			ts := httptest.NewServer(test.Route(handlers...))
 			defer ts.Close()
@@ -128,26 +122,17 @@ func TestGetUser(t *testing.T) {
 			os.Setenv("MOCK_API_URL", ts.URL)
 			defer os.Setenv("MOCK_API_URL", oldURL)
 
-			param := url.Values{}
-			for k, v := range tc.params {
-				param.Add(k, v)
-			}
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://localhost/?"+param.Encode(), nil)
-			Get(w, r)
-			got := map[string][]Entry{}
-			t.Logf(w.Body.String())
-			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
-				t.Errorf("error: %#v, res: %#v", err, got)
+			ids, err := GetUserID(ctx, tc.params)
+			if !assert.NoError(t, err) {
+				t.FailNow()
 			}
-			assert.Equal(t, tc.wantStatus, w.Code)
-			assert.Contains(t, keyString, got)
-			assert.ElementsMatch(t, got[keyString], tc.response)
+			assert.ElementsMatch(t, ids, tc.response)
 		})
 	}
 }
-*/
 
 func MockGetUser(w http.ResponseWriter, r *http.Request) {
 
